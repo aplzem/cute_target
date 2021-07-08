17c469e26fc6691e9e44ba94e7e217dc38ce2825
zzm <aplzem@163.com>
2021-07-09 01:04:59 +0800

    没有想法

---
+++ main.py
@@ -0,0 +1,12 @@
+import json
+daily_dict = list()
+sports = dict()
+sports01 = {'day': '07-06',
+            '持续距离': 3.02,
+            '最快配速': '5min44',
+            '达成成就': '【3分钟初尝试】'}
+sports02 = {'day': '07-08',
+            '持续距离': 3.50,
+            '最快配速': '5min33',
+            '达成成就': '【雨中漫步】'}
+print(sports01)
